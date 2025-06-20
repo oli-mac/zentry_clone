4656969eb05443dd50a0a377f2267a240b4dd41a
oli-mac <adumulugeta21@gmail.com>
2025-06-20 15:41:40 +0300

    hero section |closes #2| hero section created with some transitions still to fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Hero from './components/Hero.jsx'
+import About from './components/About.jsx'
 const App = () => {
   return (
     <main className='relative min-h-screen w-screen overflow-hidden'>
       <Hero />
-      <section className='z0 min-h-screen bg-blue-500' />
+      <About />
     </main>
   )
 }
