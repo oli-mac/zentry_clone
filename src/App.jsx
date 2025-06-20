f7816e88649e6d9872e6842d3016f9ba4245f9b6
oli-mac <adumulugeta21@gmail.com>
2025-06-20 10:05:09 +0300

    fixed the tailwindcss thiningy i hope it works

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const App = () => {
   return (
     <main>
-      <h1 className="text-3xl font-bold underline text-violet-300">
-        Hello world!
+      <h1 className="text-3xl font-bold text-orange-500 underline">
+        Hello Vite + React + Tailwind CSS!
       </h1>
-      <p className="text-lg text-orange-700 font-sans ">
-        This is a simple React app with Tailwind CSS.
+      <p className="mt-4 text-lg text-blue-700">
+        This is a simple setup to get you started with Vite, React, and Tailwind CSS.
       </p>
     </main>
   )
