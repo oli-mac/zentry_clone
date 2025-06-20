747693499edfd6a8682cae0d398357ca9b40d579
oli-mac <adumulugeta21@gmail.com>
2025-06-20 14:22:27 +0300

    hero section |closes #2| hero section created with some transitions still to fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
-
+import Hero from './components/Hero.jsx'
 const App = () => {
   return (
-    <main>
-      <h1 className="text-3xl font-bold text-orange-500 underline">
-        Hello Vite + React + Tailwind CSS!
-      </h1>
-      <p className="mt-4 text-lg text-blue-700">
-        This is a simple setup to get you started with Vite, React, and Tailwind CSS.
-      </p>
+    <main className='relative min-h-screen w-screen overflow-hidden'>
+      <Hero />
+      <section className='z0 min-h-screen bg-blue-500' />
     </main>
   )
 }
